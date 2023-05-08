962ebe6c6adffb3f433f196f3a7cccc3d5ae994c
Ioan Velev <ioanvelev46@gmail.com>
2023-05-08 16:25:59 +0300

    Create stringLength.js

---
+++ Syntax, functions and statements/stringLength.js
@@ -0,0 +1,7 @@
+function stringLength(str1, str2, str3){
+let lengthSum = str1.length + str2.length + str3.length;
+let avgLength = Math.floor(lengthSum / 3);
+console.log(lengthSum);
+console.log(avgLength);
+}
+stringLength('pasta', '5', '22.3' )
