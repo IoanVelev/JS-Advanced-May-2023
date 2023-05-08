a1f198327e27756646dc1d83388f6acfe9e975c8
Ioan Velev <ioanvelev46@gmail.com>
2023-05-08 23:02:55 +0300

    Create aggregateElements.js

---
+++ Syntax, functions and statements/aggregateElements.js
@@ -0,0 +1,12 @@
+function aggregateElements(input){
+let sumOfElements = 0;
+let sumOfReverseValues = 0;
+let concat = '';
+input.map(x => sumOfElements += x);
+input.map(x => sumOfReverseValues += 1 / x);
+input.map(x => concat += String(x));
+console.log(sumOfElements);
+console.log(sumOfReverseValues);
+console.log(concat);
+}
+aggregateElements([1, 2, 3] )
