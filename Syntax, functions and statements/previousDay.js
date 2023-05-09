eef43375cdf36aa6a776ac71fb473bc2a9c9ce4f
Ioan Velev <ioanvelev46@gmail.com>
2023-05-09 21:03:10 +0300

    Update previousDay.js

--- Syntax, functions and statements/previousDay.js
+++ Syntax, functions and statements/previousDay.js
@@ -1,7 +1,7 @@
 function previousDay(year, month, day) {
   let date = new Date(year, month, day);
   if (day === 1) {
-    date = new Date(year, month - 1);
+    date.setMonth(date.getMonth() - 1)
     let day = new Date(year, month - 1, 0).getDate();
     console.log(`${date.getFullYear()}-${date.getMonth()}-${day}`);
   } else {
