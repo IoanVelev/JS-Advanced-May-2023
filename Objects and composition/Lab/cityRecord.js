920d3f2709f89e944468cd08a8cc04fcf80fd2ad
Ioan Velev <ioanvelev46@gmail.com>
2023-05-15 15:39:58 +0300

    Create cityRecord.js

---
+++ Objects and composition/Lab/cityRecord.js
@@ -0,0 +1,9 @@
+function cityRecord(name, population, treasury) {
+  let record = {
+    name,
+    population,
+    treasury,
+  };
+  return record;
+}
+console.log(cityRecord("Tortuga", 7000, 15000));
