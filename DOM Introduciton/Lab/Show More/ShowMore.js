842de0b23c63c6534106d7fddbf2fc327c66aef6
Ioan Velev <ioanvelev46@gmail.com>
2023-05-20 22:33:03 +0300

    add tasks

---
+++ DOM Introduciton/Lab/Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let linkTextEl = document.getElementById('more');
+    let replaceTextEl = document.getElementById('text');
+    replaceTextEl.style.display = 'inline';
+    linkTextEl.style.display = 'none';
+}
