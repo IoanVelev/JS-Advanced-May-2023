980467a85def65bb58cf5187d0133ae96f35336d
Ioan Velev <ioanvelev46@gmail.com>
2023-05-20 17:01:49 +0300

    Update heroes.js

--- Objects and composition/Exercises/heroes.js
+++ Objects and composition/Exercises/heroes.js
@@ -1,48 +1,32 @@
 function solve() {
-  // Create mage object
-  let mageHero = {
-    // proto for mage
-    mageProto: {
-      cast: function (spell) {
-        this.mana -= 1;
-        console.log(`${this.name} cast ${spell}`);
-      },
-    },
-    // Mage constructor
-    mage: function (name) {
-      let mageObj = {
-        ...this.mageProto,
-        name,
-        mana: 100,
-        health: 100,
-      };
-      return mageObj;
-    },
-  };
-  // fighter object
-  let fighterHero = {
-    // proto for fighter
-    fighterProto: {
-      fight: function () {
+let health = 100;
+let stamina = 100;
+let mana = 100;
+let create = {
+  fighter: function(name) {
+    return (obj = {
+      name,
+      health,
+      stamina,
+      fight: function() {
         this.stamina -= 1;
         console.log(`${this.name} slashes at the foe!`);
       },
-    },
-    // fighter constructor
-    fighter: function (name) {
-      let fighterObj = {
-        ...this.fighterProto,
-        name,
-        health: 100,
-        stamina: 100,
-      };
-      return fighterObj;
-    },
-  };
-  let allHeroes = {
-    ...mageHero,
-    ...fighterHero,
-  };
-  return allHeroes;
+    });
+  },
+  mage: function(name) {
+    return(obj = {
+      name,
+      health,
+      mana,
+      cast: function(spellName){
+        this.mana -= 1;
+        console.log(`${this.name} cast ${spellName}`
+        );
+      }
+    })
+  }
+}
+return create;
 }
 
