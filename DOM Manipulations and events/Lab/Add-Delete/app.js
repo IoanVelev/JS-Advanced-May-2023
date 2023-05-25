8b992c3048eea9197b7c6fa8f3f632c61eabbc91
Ioan Velev <ioanvelev46@gmail.com>
2023-05-25 17:35:02 +0300

    add tasks

---
+++ DOM Manipulations and events/Lab/Add-Delete/app.js
@@ -0,0 +1,15 @@
+function addItem() {
+    let addedInput = document.getElementById('newItemText');
+    let listedElements = document.getElementById('items');
+    let liElement = document.createElement('li');
+    liElement.textContent = addedInput.value;
+    addedInput.value = '';
+    let deletedElement = document.createElement('a');
+    deletedElement.href = '#';
+    deletedElement.textContent = '[Delete]';
+    deletedElement.addEventListener('click', (el) => {
+        el.currentTarget.parentElement.remove();
+    })
+    listedElements.appendChild(liElement);
+    liElement.appendChild(deletedElement);
+}
