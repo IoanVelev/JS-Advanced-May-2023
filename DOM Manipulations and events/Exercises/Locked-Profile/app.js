999a080383d8f546818f8268447eda6cb8e3f4e9
Ioan Velev <ioanvelev46@gmail.com>
2023-05-28 17:52:11 +0300

    add tasks

---
+++ DOM Manipulations and events/Exercises/Locked-Profile/app.js
@@ -0,0 +1,19 @@
+function lockedProfile() {
+    Array.from(document.querySelectorAll('.profile button')).forEach(button => button.addEventListener('click', onClick));
+
+    function onClick(event) {
+        let parentElement = event.target.parentElement;
+        let isUnlocked = parentElement.querySelector('input[value="unlock"]').checked;
+        if (isUnlocked) {
+            let hiddenInfo = parentElement.querySelector('div');
+
+            if (event.target.textContent === 'Show more') {
+                event.target.textContent = 'Hide it';
+                hiddenInfo.style.display = 'block';
+            } else {
+                event.target.textContent = 'Show more';
+                hiddenInfo.style.display = 'none'
+            }
+        }
+    }
+}
