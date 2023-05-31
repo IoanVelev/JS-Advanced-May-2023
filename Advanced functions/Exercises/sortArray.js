f11012c299bc6f53a302e470c25e96a2562cb98b
Ioan Velev <ioanvelev46@gmail.com>
2023-05-31 16:33:15 +0300

    Update sortArray.js

--- Advanced functions/Exercises/sortArray.js
+++ Advanced functions/Exercises/sortArray.js
@@ -1,9 +1,4 @@
 function sortArray(arr, order) {
-  if (order === "asc") {
-    arr = arr.sort((a, b) => a - b);
-  } else {
-    arr = arr.sort((a, b) => b - a);
-  }
-  return arr;
+  return order === 'asc' ? arr.sort((a,b) => a - b) : arr.sort((a,b) => b - a);
 }
 
