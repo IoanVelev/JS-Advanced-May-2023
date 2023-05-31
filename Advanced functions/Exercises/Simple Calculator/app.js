81d9f9b87648a38cac4124163ef0c267377912a3
Ioan Velev <ioanvelev46@gmail.com>
2023-05-31 19:10:15 +0300

    add tasks

---
+++ Advanced functions/Exercises/Simple Calculator/app.js
@@ -0,0 +1,22 @@
+function calculator() {
+   let input1;
+   let input2;
+   let result;
+   debugger;
+   function init(selector1, selector2, resultSelector) {
+    input1 = document.querySelector(selector1);
+    input2 = document.querySelector(selector2);
+    result = document.querySelector(resultSelector)
+   }
+   function add() {
+    result.value = Number(input1.value) + Number(input2.value);
+   }
+   function subtract() {
+    result.value = Number(input1.value) - Number(input2.value);
+   }
+   return {
+    init,
+    add,
+    subtract,
+   }
+}
